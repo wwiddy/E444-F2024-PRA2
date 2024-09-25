83958edb356cccc47e638b32477506f04a87ff19
wwiddy <b.widaatalla@mail.utoronto.ca>
2024-09-25 11:17:37 -0400

    Example 2-2 Reproduction

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-#Example 2-1. hello.py: A complete Flask application
+#Example 2-2. hello.py: Flask application with a dynamic route
 
 from flask import Flask
 app = Flask(__name__)
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
